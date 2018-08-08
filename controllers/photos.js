ae40f335847254f752b8e0b3abd2b815c28f962c
Katie Truong <katietruong.ucdavis@gmail.com>
2018-08-08 11:08:34 -0700

    Implement working CRUD APIs

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,17 +1,16 @@
 const db = require('../models');
 
 module.exports = {
-  index: function(req, res, next) {
+  get(req, res, next) {
     /*
       method: GET
       route: '/api/photos/:id'
     */
-    console.log('MY PHOTO COMPONENT WAS HIT');
     const roomId = req.params.id;
 
     db.Photo.find({ room_id: roomId })
       .exec()
-      .then(function(photos) {
+      .then((photos) => {
         if (!photos || !photos.length) {
           return next();
         }
@@ -19,5 +18,49 @@ module.exports = {
         res.json({ results: photos });
       })
       .catch(next);
-  }
+  },
+
+  post(req, res) {
+    /*
+      method: POST
+      route: '/api/photos/:id'
+    */
+    const newPhoto = new db.Photo();
+    newPhoto.room_id = req.params.id;
+    newPhoto.description = req.params.description;
+    newPhoto.verified = req.params.verified;
+    newPhoto.photo_url = req.params.photo_url;
+    newPhoto.save((err, photo) => {
+      if (err) {
+        console.log('Error inserting photo!')
+      }
+      res.send('Successful posting photo!')
+    })
+  },
+
+  put(req, res) {
+    /*
+      method: PUT
+      route: '/api/photos/:id'
+    */
+    const roomId = req.params.id;
+    db.Photo.updateOne({ room_id: roomId }, req.body, (err) => {
+      if (err) return res.status(405).send(err);
+      res.send('Success updating data!');
+    })
+  },
+
+  delete(req, res) {
+    /*
+      method: DELETE
+      route: '/api/photos/:id'
+    */
+    const roomId = req.params.id;
+    db.Photo.deleteOne({ room_id: roomId }, (err) => {
+      if (err) {
+        console.log('Error deleting data!')
+      }
+      res.send('Success deleting data!');
+    })
+  },
 };
