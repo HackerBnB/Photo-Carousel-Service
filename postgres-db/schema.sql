9c084288e8599ad1f9933ac250f81ce93c68c898
Katie Truong <katietruong.ucdavis@gmail.com>
2018-08-10 15:16:00 -0700

    Create schema to import data to PSQL database

--- postgres-db/schema.sql
+++ postgres-db/schema.sql
@@ -1,6 +1,6 @@
 -- SELECT pg_terminate_backend(pid)
 -- FROM pg_stat_get_activity(NULL::integer)
--- WHERE datid=(SELECT oid from pg_database where datname = 'sample');
+-- WHERE datid=(SELECT oid from pg_database where datname = 'homely');
 
 DROP DATABASE IF EXISTS homely;
 
@@ -8,20 +8,45 @@ CREATE DATABASE homely;
 
 \c homely;
 
-CREATE TABLE photos(
-  room_id TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE rooms(
+  room_id INT NOT NULL PRIMARY KEY,
   room_name VARCHAR(120) NOT NULL,
-  photo_url VARCHAR(120) NOT NULL,
-  description VARCHAR(200) NOT NULL,
-  verified BOOLEAN NOT NULL);
+  verified BOOLEAN NOT NULL
+);
 
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_1.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_2.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_3.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_4.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_5.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_6.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_7.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_8.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_9.csv' WITH DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/data_10.csv' WITH DELIMITER ',' CSV HEADER;
+CREATE TABLE photos(
+  photo_id INT NOT NULL PRIMARY KEY,
+  photo_url VARCHAR(120) NOT NULL,
+  photo_description VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE rooms_photos(
+  id INT NOT NULL PRIMARY KEY,
+  room_id INTEGER NOT NULL,
+  photo_id INTEGER NOT NULL
+);
+
+-- \timing;
+
+COPY rooms FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_1.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_2.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_1.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_2.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_3.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_4.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_5.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_6.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_7.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_8.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_9.csv' WITH DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/photos_10.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_1.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_2.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_3.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_4.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_5.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_6.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_7.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_8.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_9.csv' WITH DELIMITER ',' CSV HEADER;
+COPY rooms_photos FROM '/Users/katietruong/Documents/HR/SDC/Photo-Carousel-Service/data/rooms_photos_10.csv' WITH DELIMITER ',' CSV HEADER;
