607aa55e0b6b08f4d8c504cbc14c62159ef98b80
Katie Truong <katietruong.ucdavis@gmail.com>
2018-08-10 12:19:21 -0700

    Modify data generator to have more than one photo per room

--- data-generator.js
+++ data-generator.js
@@ -9,20 +9,51 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
-const photos = (numofEntries) => {
-  let csv = 'room_id, room_name, photo_url, description, verified';
+const rooms = (numofEntries) => {
+  let csv = 'room_id, room_name, verified';
   for (let i = 1; i <= numofEntries; i++) {
-    let room_id = i;
-    let room_name = 'room' + i.toString();
-    let photo_url = PHOTO_URL + getRandomIntInclusive(1, 75) + '.jpg';
-    let description = faker.lorem.sentence();
-    let verified = !Math.floor(Math.random() * 2);
-    csv = csv + `\n ${room_id}, ${room_name}, ${photo_url}, ${description}, ${verified}`;
+    const roomId = i;
+    const roomName = 'room' + i.toString();
+    const verified = !Math.floor(Math.random() * 2);
+    csv = csv + `\n ${roomId}, ${roomName}, ${verified}`;
+  }
+  return csv;
+};
+
+fs.writeFile('data/sample_rooms_1.csv', rooms(100), 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('Success!')
+});
+
+
+const photos = (numofEntries) => {
+  let csv = 'photo_id, photo_url, photo_description';
+  for (let i = 1; i <= numofEntries; i++) {
+    const photoId = i;
+    const photoUrl = PHOTO_URL + getRandomIntInclusive(1, 75) + '.jpg';
+    const photoDescription = faker.lorem.sentence();
+    csv = csv + `\n ${photoId}, ${photoUrl}, ${photoDescription}`;
+  }
+  return csv;
+};
+
+fs.writeFile('data/sample_photos_1.csv', photos(100), 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('Success!')
+});
+
+const roomPhotos = (numofEntries) => {
+  let csv = 'id, roomId, photoId';
+  for (let i = 1; i <= numofEntries; i++) {
+    const id = i;
+    const roomId = getRandomIntInclusive(1, 100);
+    const photoId = PHOTO_URL + getRandomIntInclusive(1, 75) + '.jpg';
+    csv = csv + `\n ${id}, ${roomId}, ${photoId}`;
   }
   return csv;
 }
 
-fs.writeFile('data/sample.csv', photos(100), 'utf-8', (err) => {
+fs.writeFile('data/sample_rooms_photos_1.csv', roomPhotos(1000), 'utf-8', (err) => {
   if (err) throw err;
   console.log('Success!')
-})
+});
